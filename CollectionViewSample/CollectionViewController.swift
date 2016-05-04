34ecc0e063e4363b1ad5d788c7136ce4b03d625e
markfour <axhfaro@gmail.com>
2016-05-04 22:30:30 +0900

    Add CollectionViewController

---
+++ CollectionViewSample/CollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewSample
+//
+//  Created by HayashidaKazumi on 2016/05/04.
+//  Copyright © 2016年 HayashidaKazumi. All rights reserved.
+//
+
+import Foundation
